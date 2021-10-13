18414fc562265cb6747d161465ce43b3c3f7248d
Shirin <shirinetezazian@gmail.com>
2021-10-13 15:32:58 -0600

    Add dummy data

---
+++ client/src/dummyData.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    city: 'Boulder',
+    item: 'glass bottle',
+    recycalable: 'yes'
+  },
+  {
+    city: 'Boulder',
+    item: 'aluminum can',
+    recycalable: 'yes'
+  }
+];
+
+export default data;
